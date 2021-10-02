c21e1bb2e34ed966bb2309708a4bfa5f1676b144
Gmatieso <matiesogeoffrey@gmail.com>
2021-10-03 01:23:59 +0300

    Added string dictionaries to the file

--- array.py
+++ array.py
@@ -1,2 +1,6 @@
 
-"""Reverse array in place
+"""
+Reverse array in place
+write a function reverseArray(A) that takes in an Array A and reverse it, without using another array in place
+A = [20, 10, 6, 8]
+""""
